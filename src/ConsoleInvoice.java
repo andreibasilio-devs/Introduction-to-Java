216a579fd7c8a367bca0b7a8a24e43c34315fd55
Andrei Basilio <andreibasilio4090@gmail.com>
2026-09-14 19:43:56 +0800

    First commit.

---
+++ src/ConsoleInvoice.java
@@ -0,0 +1,25 @@
+// Basilio, Andrei Kyle I.
+// 2nd year BSIT majoring in Web Technology
+// Sept 14, 2026
+// Console based Invoice program
+public class ConsoleInvoice {
+    public static void main(String[] args) {
+        //This is my program showcasing the use of print statements, \n, \", and \t.
+        System.out.println("\n\"Andrei's Sari-Sari Store\""); // Using print statements to print the hardcoded outputs.
+        System.out.println("No.94 Purok 5, Los Bolivard, Baguio City, Philippines.\n"); // Using \n and \t for formatting purposes.
+        System.out.println("1.Kopiko Blanca\t\t\t\t\tPHP 12\t\t3x\tPHP 36");
+        System.out.println("2.Gardenia Whole Wheat Bread\tPHP 105\t\t2x\tPHP 210");
+        System.out.println("3.Clear Men Shampoo\t\t\t\tPHP 10\t\t4x\tPHP 40");
+        System.out.println("4.Eggs\t\t\t\t\t\t\tPHP 9\t\t10x\tPHP 90");
+        System.out.println("5.Star Margarine\t\t\t\tPHP 32\t\t3x\tPHP 64");
+        System.out.println("6.Jasmine Bachelor Rice (25KG)\tPHP 1320\t2x\tPHP 2640");
+        System.out.println("7.Pasta Express Carbonara\t\tPHP 18\t\t4x\tPHP 72");
+        System.out.println("8.Tender Juicy Hotdog (1KG)\t\tPHP 150\t\t2x\tPHP 300");
+        System.out.println("9.Fita 10 Packs\t\t\t\t\tPHP 70\t\t3x\tPHP 210");
+        System.out.println("10.Selecta Ice Cream\t\t\tPHP 230\t\t3x\tPHP 690");
+        System.out.println("--------------------------------------------------------");
+        System.out.print("Total:\t\t\t\t\t\t\t\t\t\t\t"); //Using a print statement to print the next output on the same line.
+        System.out.println("PHP 4352"); //Prints the hardcoded total.
+        System.out.println("\nThank you for buying!"); // Greetings.
+    }
+}
